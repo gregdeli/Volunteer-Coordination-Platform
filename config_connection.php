0736da271f91cc8de43385c629693dae6f8699e8
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2023-11-27 12:05:36 +0200

    latest

--- config_connection.php
+++ config_connection.php
@@ -1,10 +1,20 @@
 <?php
+    $db_server = "localhost";
+    $db_user = "root"; 
+    $db_pass = "";
+    $db_name = "volunteer_coordination";
+    $conn = ""; 
 
-$connection = mysqli_connect('localhost','usename','password','db_name');
-if($connection){
-    die("Connection failed: ". mysqli_connect_error());
-}
+    try{
+        $conn = mysqli_connect($db_server, $db_user, 
+                               $db_pass, $db_name);
+    }
+    catch(mysqli_sql_exception){
+        echo "Could not connect! <br>";
+    }
 
-
+    if($conn){
+        echo "You are connected! <br>";
+    }
 ?>
 
--- login.php
+++ login.php
@@ -9,22 +9,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
    $username = mysqli_real_escape_string($conn, $_POST['username']);
    $password = md5($_POST['password']);
 
-   $query = " SELECT * FROM db_name WHERE name = '$username' && password = '$password' ";
+   $query = " SELECT * FROM users WHERE username = '$username' && password = '$password' ";
 
-   $result = mysqli_query($connection, $query);
-
+   $result = mysqli_query($conn, $query);
+  
    if(mysqli_num_rows($result) > 0){
 
       $row = mysqli_fetch_array($result);
 
-      if($row['user_type'] == 'admin'){
+      if($row['role'] == 'ADMIN'){
 
-         $_SESSION['admin_name'] = $row['name'];
+         $_SESSION['admin_name'] = $row['full_name'];
          header('location:admin_page.php');
 
-      }elseif($row['user_type'] == 'user'){
+      }elseif($row['role'] == 'RESCUER'){
 
-         $_SESSION['user_name'] = $row['name'];
+         $_SESSION['rescuer_name'] = $row['full_name'];
          header('location:user_page.php');
 
       }
