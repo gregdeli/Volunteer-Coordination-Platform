28c3ae04967239bb3915fe5ae11523b8432bf9da
elamoresi <zikossp@gmail.com>
2023-11-30 20:37:48 +0200

    Styling map

--- js/header.js
+++ js/header.js
@@ -1,5 +1,6 @@
-var filePath = 'header.html';
-
-fetch(filePath)
-    .then(response => {return response.text();})
+function header_footer() {
+    fetch('header.html').then(response => {return response.text();})
     .then(fileContents => {document.getElementById('header').innerHTML = fileContents;});
+    fetch('footer.html').then(response => {return response.text();})
+    .then(fileContents => {document.getElementById('footer').innerHTML = fileContents;});
+  }
