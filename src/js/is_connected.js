350c55f8afa97796c0bd6b9b2d819c37367ab624
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2023-12-08 09:37:45 +0200

    fixed some issues with directories

--- src/js/is_connected.js
+++ src/js/is_connected.js
@@ -3,7 +3,7 @@ $.ajax({
     url: "/src/php/is_connected.php",
     success: function(response) {
         if (response.trim() === "notConnected") {
-            window.location.replace("/src/pages/index.html"); 
+            window.location.replace("/src/pages/login/index.html"); 
         } else {
             document.getElementById("admin_name").innerHTML = response;
             console.log("connected");
