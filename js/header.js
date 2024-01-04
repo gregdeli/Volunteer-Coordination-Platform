8a01f48e122de2765a96b5ad50227fdd51c93b28
spyros-zikos <83170047+spyros-zikos@users.noreply.github.com>
2024-01-04 17:45:58 +0200

    Delete js directory

--- js/header.js
+++
@@ -1,6 +0,0 @@
-function header_footer() {
-    fetch('header.html').then(response => {return response.text();})
-    .then(fileContents => {document.getElementById('header').innerHTML = fileContents;});
-    fetch('footer.html').then(response => {return response.text();})
-    .then(fileContents => {document.getElementById('footer').innerHTML = fileContents;});
-  }
