097d8fec67f21be4debff65e4c93404e303b537a
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2024-01-16 19:34:21 +0200

    delete src

--- src/php/is_connected.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    if(!isset($_COOKIE["admin"])){
-        echo json_encode(['value' => true, 'message' => '/src/pages/login/index.html']);
-    }else{
-        echo json_encode(['value' => false, 'message' => '']);
-    }
-
-?>
--- src/php/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-    setcookie("admin", "", time() - 3600);
-    header("Location: /src/pages/login/index.html");
-?>
