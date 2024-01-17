e05207804c6432ea3ec95ca46889ba6593976929
Kall-K <84861402+Kall-K@users.noreply.github.com>
2024-01-17 20:04:17 +0200

    Add files via upload

--- src/js/admin/create_announcement.js
+++ src/js/admin/create_announcement.js
@@ -1,110 +1,102 @@
-var dynamicOptions = [];
-
 function getCategories() {
     return new Promise((resolve) => {
-        function onSuccess(response) {
-            dynamicOptions = JSON.parse(response);
+        fetch('/src/php/admin/create_announcement.php?func=1')
+        .then(response=>response.text())
+        .then(categoriesLoaded=>{
+            let categories = JSON.parse(categoriesLoaded);
+            setCategories(categories);
             resolve();
-        }
-  
-        const request = $.ajax({
-            url: "/src/php/admin/create_announcement.php",
-            type: "GET",
-            data: {func: 1}
-        });
-      
-        request.done(onSuccess);
-        request.fail(function (error) {
-            console.log("ERROR");
-        });
+        })
+        .catch(error=>{console.log('Error:'+error)});
     });
 }
 
+async function initializeDropDown() {
+    await getCategories();
+    await getItems();
+    setItems();
+}
 
-function dynamicDropDown(){
-    // await getCategories();
-    var dropdown = document.getElementById('dynamicdropdown');
+async function refreshDropDown() {
+    await getCategories();
+    changeListItems();
+}
 
-    dynamicOptions.forEach(function(optionText) {
+function setCategories(categories) {
+    var dropdown = document.getElementById('dropdown');
+    categories.forEach(function(category) {
         var option = document.createElement('option');
-        option.value = optionText.id; 
-        option.text = optionText.name;
+        option.value = category.id; 
+        option.text = category.name;
         dropdown.add(option);
     });
 }
 
-
 var items = [];
 
-function getItems(selectedCategory){
+function getItems() {
     return new Promise((resolve) => {
-        function onSuccess(response) {
-            console.log(response);
-            items =  JSON.parse(response);
+        var selectedCategory = document.getElementById('dropdown').value;
+        fetch('/src/php/admin/create_announcement.php?func=2&cat='+selectedCategory)
+        .then(response=>response.text())
+        .then(itemsLoaded=>{
+            items = JSON.parse(itemsLoaded);
             resolve();
+        })
+        .catch(error=>{console.log('Error:'+error)});
+    });
+    
+}
+
+function setItems() {
+    if (items.id != 0) {
+        items.forEach(function(item) {
+            var itemsList = document.getElementById('itemslist');
+            var listItem = document.createElement('li');
+
+            var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = item.id;
+            listItem.appendChild(checkbox);
+
+            var announcementTextSpan = document.createElement('span');
+            announcementTextSpan.textContent = item.name;
+            listItem.appendChild(announcementTextSpan);
+            itemsList.appendChild(listItem);
+        });
+    }
+}
+
+async function changeListItems() {
+    var list = document.getElementById('itemslist');
+    await getItems();
+
+    var liElements = list.getElementsByTagName('li');
+    var lenItems = items.length;
+    var lenLi = liElements.length;
+    var liArray = Array.from(liElements);
+
+    if (items.id == 0) {      
+        for (let i = 0; i < lenLi; i++) 
+            list.removeChild(liArray[i]);        
+    } else {
+        for (let i = 0; i < Math.min(lenLi, lenItems); i++) {
+            liArray[i].innerHTML = '<input type="checkbox" id="' + items[i].id + '">' + items[i].name;
+            document.getElementById(items[i].id).checked = isAlreadyChecked(items[i].id);
         }
-        const request = $.ajax({
-            url: "/src/php/admin/create_announcement.php",
-            type: "GET",
-            data: {func: 2, cat: selectedCategory}
-        });
-        request.done(onSuccess);
-        request.fail(function(error) {
-            console.log("ERROR");
-        });
-    });
-}
 
+        for (let i = lenItems; i < lenLi; i++) {
+            list.removeChild(liArray[i]);
+        }
 
-async function itemsList(){
-    var selectedCategory = document.getElementById("dynamicdropdown").value;
-    console.log(selectedCategory);
-    await getItems(selectedCategory);
-    console.log(items,"itemsList");
-
-    items.forEach(function(item) {
-        var itemsList = document.getElementById("itemslist");
-        var listItem = document.createElement("li");
-
-        var checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.id = item.id;
-        listItem.appendChild(checkbox);
-
-        var announcementTextSpan = document.createElement("span");
-        announcementTextSpan.textContent = item.name;
-        listItem.appendChild(announcementTextSpan);
-        itemsList.appendChild(listItem);
-    });
-}
-
-
-async function changeItemsList(){
-    var list = document.getElementById("itemslist");
-    var selectedCategory = document.getElementById("dynamicdropdown").value;
-    await getItems(selectedCategory);
-
-    var liElements = list.getElementsByTagName("li");
-
-    for (var i = 0; i < Math.min(liElements.length, items.length); i++) {
-        liElements[i].innerHTML = '<input type="checkbox" id="' + items[i].id + '">' + items[i].name;
-
-        document.getElementById(items[i].id).checked = isAlreadyChecked(items[i].id);
-        console.log(isAlreadyChecked(items[i].id));
+        for (let i = lenLi; i < lenItems; i++) {
+            var newLi = document.createElement('li');
+            newLi.innerHTML = '<input type="checkbox" id="' + items[i].id + '">' + items[i].name;
+            list.appendChild(newLi);
+            document.getElementById(items[i].id).checked = isAlreadyChecked(items[i].id);
+        }
     }
 
-    for (var i = items.length; i < liElements.length; i++) {
-        list.removeChild(liElements[i]);
-    }
-
-    for (var i = liElements.length; i < items.length; i++) {
-        var newLi = document.createElement("li");
-        newLi.innerHTML = '<input type="checkbox" id="' + items[i].id + '">' + items[i].name;
-        list.appendChild(newLi);
-
-        document.getElementById(items[i].id).checked = isAlreadyChecked(items[i].id);
-        console.log(isAlreadyChecked(items[i].id));
-    }
     
 }
 
@@ -118,7 +110,7 @@ function isAlreadyChecked(item){
 }
 
 
-const checkedCheckboxes = [];
+var checkedCheckboxes = [];
 
 function storeCheckedCheckboxes() {
     var checkboxes = document.querySelectorAll('input[type="checkbox"]');
@@ -132,31 +124,32 @@ function storeCheckedCheckboxes() {
         else if (!(checkbox.checked) && isPresent){
             let index = checkedCheckboxes.indexOf(checkbox.id);
             checkedCheckboxes.splice(index, 1);
-            console.log(index,"deleted");
         }
     });
-
-    console.log('Checked Checkboxes:', checkedCheckboxes);
 }
 
 
 function createAnnouncement(){
-   
-    function onSuccess(response) {
-        console.log(response);
+    if (checkedCheckboxes.length != 0) {
+        fetch('/src/php/admin/create_announcement.php?func=3&items='+checkedCheckboxes)
+        .then(response=>response.text())
+        .then(messageReturned=>{        
+            alert(JSON.parse(messageReturned).response);
+            document.querySelectorAll('input[type="checkbox"]').forEach( function(checkbox) {
+                checkbox.checked = false;
+            })
+            checkedCheckboxes = [];
+            var dropdown = document.getElementById("dropdown");
+            while (dropdown.options.length > 0) {
+                dropdown.remove(0);
+            }
+            refreshDropDown();
+        })
+        .catch(error=>{console.log('Error:'+error)});
+    } else {
+        alert('There is no Item Selected!');
     }
-
-    const request = $.ajax({
-        url: "/src/php/admin/create_announcement.php",
-        type: "POST",
-        data: {func: 3, items: checkedCheckboxes}//JSON.stringify({ data: dataArray })
-    });
-  
-    request.done(onSuccess);
-    request.fail(function (error) {
-        console.log("ERROR");
-    });
-
+    
 }
 
 
