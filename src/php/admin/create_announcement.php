8f47b1a0f1155b20db23442d142edf8e30769536
Kall-K <84861402+Kall-K@users.noreply.github.com>
2024-01-17 20:07:32 +0200

    Add files via upload

--- src/php/admin/create_announcement.php
+++ src/php/admin/create_announcement.php
@@ -1,31 +1,34 @@
 <?php
 include('../config_connection.php');
 
-if($_SERVER["REQUEST_METHOD"] == "GET"){
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $selected_func = $_GET["func"];
-}else if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $selected_func = $_POST["func"];
-}else{
+    if ($selected_func == 1) {
+        getCategory($conn);
+    } else if ($selected_func == 2) {
+        $category = $_GET["cat"];
+        getItem($conn, $category);
+    } else if ($selected_func == 3) {
+        $items = $_GET["items"];
+        $items = explode(',', $items);
+        if ($items[0] == '') 
+            echo json_encode(array('response' => 'There is no Item Selected!'));
+        else 
+            createAnnouncement($conn, $items);
+    } else {
+        echo "SELECTION_ERROR";
+    }
+} else {
     echo "REQUEST_METHOD_ERROR";
 }
 
-if ($selected_func == 1){
-    getCategory($conn);
-}else if($selected_func == 2){
-    $category = $_GET["cat"];
-    getItem($category, $conn);
-}else if($selected_func == 3){
-    $items = $_POST["items"];
-    createAnnouncement($items, $conn);
-}else{
-    echo "SELECTION_ERROR";
-}
+
 
 function getCategory($conn){
     $query = " SELECT * FROM category ";
     $result = mysqli_query($conn, $query);
 
-    while($row = mysqli_fetch_array($result)){
+    while($row = mysqli_fetch_array($result)) {
         $id = $row['id'];
         $name = $row['name'];
 
@@ -35,28 +38,34 @@ function getCategory($conn){
     echo json_encode($categories);
 }
 
-function getItem($category, $conn){
+function getItem($conn, $category) {
     $query = " SELECT id, name FROM item WHERE category_id = $category ";
     $result = mysqli_query($conn, $query);
 
-    while($row = mysqli_fetch_array($result)){
-        $id = $row['id'];
-        $name = $row['name'];
+    if (mysqli_num_rows($result) == 0) {
+        echo json_encode(array("id" => 0,
+                               "name" => 'None'));
+    } else {
+        while($row = mysqli_fetch_array($result)){
+            $id = $row['id'];
+            $name = $row['name'];
 
-        $items[] = array("id" => $id,
-                        "name" => $name);
+            $items[] = array("id" => $id,
+                            "name" => $name);
+        }
+        echo json_encode($items);
     }
-    echo json_encode($items);
+    
 }
 
-function createAnnouncement($items, $conn){
-    $user = $_COOKIE["admin"];
+function createAnnouncement($conn, $items) {
+    $user = $_COOKIE['admin'];
     $date_time = date('Y-m-d H:i:s');//now()
     
     $query = " INSERT INTO announcement VALUES (NULL, $user, '$date_time') ";
     $result = mysqli_query($conn, $query);
 
-    if($result){
+    if ($result) {
         $query = " SELECT id FROM announcement WHERE date_announced = '$date_time' ";
         $result = mysqli_query($conn, $query);
         $row = mysqli_fetch_array($result);
@@ -67,7 +76,7 @@ function createAnnouncement($items, $conn){
             mysqli_query($conn, $query);
         }
     }
-
+    echo json_encode(array('response' => 'Success!'));
 
 }
 
