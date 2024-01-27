0e4b3b63b26bbdf17a6b8a35e099967406edbed3
spyros-zikos <83170047+spyros-zikos@users.noreply.github.com>
2024-01-27 12:07:01 +0200

    2 problems
    has a warehouse menu problem and a geolocation register problem

---
+++ src/php/authentication/logout.php
@@ -0,0 +1,8 @@
+<?php
+    setcookie("admin", "", time() - 3600, '/');
+    setcookie("rescuer", "", time() - 3600, '/');
+    setcookie("civilian", "", time() - 3600, '/');
+    
+    header("Location: /src/pages/authentication/log_in.html");
+    exit();
+?>
