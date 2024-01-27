abda81fc1f35444ac01a3ce405f4b8cfac5027dc
spyros-zikos <83170047+spyros-zikos@users.noreply.github.com>
2024-01-27 12:01:57 +0200

    Delete src directory

--- src/php/authentication/is_connected.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    if(!isset($_COOKIE["admin"])){
-        echo json_encode(['value' => true, 'message' => '/src/pages/authentication/log_in.html']);
-    }else{
-        echo json_encode(['value' => false, 'message' => '']);
-    }
-
-?>
--- src/php/authentication/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-    setcookie("admin", "", time() - 3600);
-    header("Location: /src/pages/authentication/log_in.html");
-?>
