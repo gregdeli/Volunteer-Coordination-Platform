5d97daf01b20b6a5e14b10c6e4f9958440fe78be
spyros-zikos <83170047+spyros-zikos@users.noreply.github.com>
2024-01-31 21:02:55 +0200

    Delete src directory

--- src/php/authentication/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    setcookie("admin", "", time() - 3600, '/');
-    setcookie("rescuer", "", time() - 3600, '/');
-    setcookie("civilian", "", time() - 3600, '/');
-    
-    header("Location: /src/html/authentication/log_in.html");
-    exit();
